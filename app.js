10ec8dadcb0ccf8e559d1dd8c49ff3bfecf1aea4
Aicholpon Bazakeeva <abazakeeva@inbox.ru>
2023-05-12 22:56:16 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+const input = document.querySelector('input');
+function isEmailValid (value){
+    return EMAIL_REGEXP.test(value);
+}
+function onInput(){
+  if (isEmailValid(input.value)) {
+    input.style.borderColor = 'blue';
+  }
+    else{
+    input.style.borderColor = 'red';
+}
+}
+input.addEventListener('input', onInput);
+
+
+
+
+
